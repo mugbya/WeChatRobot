c6b4e511157fb75e0bf3c4dc224bae0a7dce1100
亦恕 <mugbya@gmail.com>
2024-02-21 20:59:19 +0800

    修改
    Signed-off-by: 亦恕 <mugbya@gmail.com>

--- business/room_data.py
+++ business/room_data.py
@@ -5,9 +5,9 @@
 # 43277207753@chatroom, 48193485317@chatroom, 44395519219@chatroom, 39266269224@chatroom,]
 
 room_data_dit = {
-    "43277207753@chatroom": ["wxid_tjwqcw2e1w0a21"],  # 魔都群
+    "43277207753@chatroom": ["wxid_tjwqcw2e1w0a21", "xc111849", "jingfeng-chenhuan", "Xy00000719"],  # 魔都群
     "48193485317@chatroom": ["wxid_tjwqcw2e1w0a21"],  # 3个人的测试群
-    "44395519219@chatroom": ["wxid_tjwqcw2e1w0a21"],  # 鲜成拉的群
+    "44395519219@chatroom": ["wxid_tjwqcw2e1w0a21", "xc111849"],  # 鲜成拉的群
     "39266269224@chatroom": ["wxid_tjwqcw2e1w0a21"],  # 天府率土征服群
 }
 
