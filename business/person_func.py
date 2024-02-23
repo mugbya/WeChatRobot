3e317d1e23e99d8b74c7176c2315dc94e5c1e4ab
亦恕 <mugbya@gmail.com>
2024-02-23 10:48:39 +0800

    修改
    Signed-off-by: 亦恕 <mugbya@gmail.com>

--- business/person_func.py
+++ business/person_func.py
@@ -13,20 +13,12 @@ class PersonFunc(object):
         self.LOG = logging.getLogger("Robot")
 
     @staticmethod
-    def print_menu(msg, robot):
-        text, user = BaseFunc.command_common(msg)
-        if text in ["功能", "功能列表", "大橘功能"]:
-            robot.sendTextMsg(person_menu, user)
-            return True
-        return False
-
-    @staticmethod
     def handler_command(msg, robot):
         text, user = BaseFunc.command_common(msg)
         if text in ["功能", "功能列表", "大橘功能"]:
             robot.sendTextMsg(person_menu, user)
             return True
-        if text == ["试用", "使用", "使用大橘"]:
+        if text in ["试用", "使用", "使用大橘"]:
             robot.wcf.add_chatroom_members(room_data.test_room_id, msg.sender)
 
         return False
